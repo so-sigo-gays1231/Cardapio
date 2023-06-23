9a0a377b372531a0e11fa78faef31ed99513977c
so-sigo-gays1231 <136845014+so-sigo-gays1231@users.noreply.github.com>
2023-06-23 15:28:00 -0300

    Add files via upload

---
+++ Cardapio-main/Cardapio.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+
+    <style>
+        .main{
+            
+            display: flex;
+            flex-direction: row;
+        }
+    </style>
+</head>
+<body>
+    
+<div class="main">
+<div class="card" style="width:18rem;">
+  <img src="https://pastapizza.com.br/wp-content/uploads/2021/06/Gorgonzola-1536x1007.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">GORGONZOLA</h5>
+    <p class="card-text">A melhor pizza que vc ja viu com gosto otimo! tamanho familia </p>
+    <a href="#" class="btn btn-primary">R$: 150,00 </a>
+  </div>
+
+  <div class="card">
+  
+  <img src="https://www.opovo.com.br/_midias/jpg/2023/01/23/818x460/1_whatsapp_image_2023_01_18_at_12_23_27-20897249.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">PIZZA DE OURO</h5>
+    <p class="card-text"> Compra mais de 5 pizzas diferentes e ganhe uma extra Pizza Dourada </p>
+    <a href="#" class="btn btn-primary">Premium</a>
+</div>
+</div>
+</body>
+</html>
